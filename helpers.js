450ae2fe8a2b67873c651f91ce7f9e63d7e4bdd4
Daniel Nascimento <danielhn@gmail.com>
2020-08-06 20:24:45 +0000

    Commented about the helper functions

--- helpers.js
+++ helpers.js
@@ -1,17 +1,20 @@
 const bcrypt = require('bcrypt');
 
+// Generate a random cookie key
 const generateCookieKey = () => {
   return Math.random()    //  Returns a random number between 0 and 1.
   .toString(36)           //  Base36 encoding; use of letters with digits.
   .substring(2,10);        //  Returns the part of the string between the start and end indexes, or to the end of the string. 
 };
 
+// Generate a random string for the user ID
 const generateRandomString = () => {
   return Math.random()    //  Returns a random number between 0 and 1.
   .toString(36)           //  Base36 encoding; use of letters with digits.
   .substring(2,8);        //  Returns the part of the string between the start and end indexes, or to the end of the string. 
 };
 
+// Asserts if an email is in the database
 const emailLookup = (email, database) => {
   for (let user of Object.keys(database)) {
     for (let item in database[user]) {
@@ -23,6 +26,7 @@ const emailLookup = (email, database) => {
   return true;
 }; 
 
+// Check if both email and password matches with the ones saved in the database
 const authenticator = (email, password, database) => {
   for (let user of Object.keys(database)) {
     for (let item in database[user]) {
@@ -34,6 +38,7 @@ const authenticator = (email, password, database) => {
   return true;
 };
 
+// Return user ID that matches the inputed email
 const getUserByEmail = (email, database) => {
   for (let user of Object.keys(database)) {
     for (let item in database[user]) {
@@ -45,6 +50,7 @@ const getUserByEmail = (email, database) => {
   return undefined;
 }; 
 
+// Builds an object with only the tiny urls assigned to a specific user
 const urlsForUser = (id, database) => {
   let urls = {};
   for (let tiny in database) {
@@ -55,6 +61,7 @@ const urlsForUser = (id, database) => {
   return urls;
 };
 
+// Exports the functions
 module.exports = {
   generateCookieKey,
   generateRandomString,
